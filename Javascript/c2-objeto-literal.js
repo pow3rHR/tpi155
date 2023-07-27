78bd18426ca33556abddf571ca3596e4837a9e0f
Diego Herrera <psystingod@gmail.com>
2023-07-27 07:17:20 -0600

    Clase 2, ejercicio 1

---
+++ Javascript/c2-objeto-literal.js
@@ -0,0 +1,8 @@
+const persona = {
+    nombre: "Diego",
+    apellido: "Herrera",
+    edad: 15
+}
+
+// Los objetos literales pueden tener valores primitivos de cualquier tipo.
+// También puede contener más objetos.
