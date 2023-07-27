2c3d91383aae3e58f69c6fa359f934706f9a7589
Diego Herrera <psystingod@gmail.com>
2023-07-27 07:56:17 -0600

    Clase 2, ejemplo 3

---
+++ Javascript/c2-objeto-constructor-1.js
@@ -0,0 +1,13 @@
+function Persona(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+console.log(Persona);
+
+/*const diego = new Persona("Diego", "Herrera");
+
+console.log(diego.nombre);*/
+
+// Los objetos constructor me proveen de una plantilla para crear muchos objetos del mismo tipo.
+// También pueden tener comportamiento y valores compuestos en su estructura.
