44773409697e1a5c58b0c85a6fe527209f87b5c0
Diego Herrera <psystingod@gmail.com>
2023-08-10 21:34:13 -0600

    Module pattern with privates was added

---
+++ Javascript/c5-patterns-5.js
@@ -0,0 +1,41 @@
+// Version más refinada del patrón módulo
+
+const RelojGlobal = (() => {
+    'use strict';
+
+    let _date = new Date();
+
+    const _mostrarHoraEsa = (pais) => {
+        let _options = {timeZone: 'America/El_Salvador'};
+        let _time = _date.toLocaleString('en-US', _options);
+        return _time;
+    }
+
+    const _mostrarHoraUsa = (pais) => {
+        let _options = {timeZone: 'America/New_York'};
+        let _time = _date.toLocaleString('en-US', _options);
+        return _time;
+    }
+
+    const imprimirHora = (pais) => {
+        switch (pais) {
+            case "El Salvador":
+                return _mostrarHoraEsa(pais)
+                break;
+            case "USA":
+                return _mostrarHoraUsa(pais)
+                break;
+            default:
+                return _date.toLocaleString("en-US", {})
+                break;
+        }
+    }
+
+    return {
+        imprimirHora
+    }
+
+})();
+
+// verificamos nuestra calculadore
+console.log(RelojGlobal.imprimirHora());
